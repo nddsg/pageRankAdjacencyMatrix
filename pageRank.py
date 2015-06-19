b309420ab30bb8ac26d63308b5475dfd51af1f76
Corey Pennycuff <cpennycu@nd.edu>
2015-06-19 13:38:42 -0400

    adding more graphs and proper labelling

--- pageRank.py
+++ pageRank.py
@@ -33,7 +33,7 @@ gml = ('adjnoun.gml',
 #  G.remove_edge(u,v)
 
 #G = nx.read_edgelist("C:\\Users\\tweninge\\Downloads\\CA-HepTh.txt")
-#G = nx.barabasi_albert_graph(1000, 1)
+G = nx.barabasi_albert_graph(1000, 1)
 #G = nx.powerlaw_cluster_graph(1000,1,.4)
 #G = nx.random_regular_graph(10, 1000)
 
@@ -44,78 +44,112 @@ gml = ('adjnoun.gml',
 #G = nx.watts_strogatz_graph(1000, 5, .25)
 #G = nx.newman_watts_strogatz_graph(1000, 3, .25)
 
-G = nx.watts_strogatz_graph(20, 4, 0)
-G.add_edge(0, int(G.number_of_nodes() / 2))
+#G = nx.watts_strogatz_graph(20, 4, 0)
+#G.add_edge(0, int(G.number_of_nodes() / 2))
 
-#pr = nx.pagerank(G, max_iter=55)
-pr = nx.pagerank(G, max_iter=255, tol=1e-16)
-#pr = nx.eigenvector_centrality(G, max_iter=500, tol=.01)
 import community
 
 dendo = community.generate_dendrogram(G, None)
-com = community.partition_at_level(dendo, len(dendo)-1 )
+com = community.partition_at_level(dendo, len(dendo) - 1)
 
 
 #gm = nx.google_matrix(G)
-maxPR = max(pr.values())
 
-f, (ax1, ax2, ax3) = plt.subplots(1,3)
+f, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
 
 #plt.imshow(gm,
 #           interpolation='nearest', origin='lower')
 #plt.show()
 
-
 x = np.zeros(G.number_of_edges() * 2)
 y = np.zeros(G.number_of_edges() * 2)
 z = np.zeros(G.number_of_edges() * 2)
 
+#pr = nx.pagerank(G, max_iter=55)
+pr = nx.pagerank(G, max_iter=255, tol=1e-16)
+maxPR = max(pr.values())
+flip = dict(zip(sorted(G.nodes(), key=lambda node: pr[node], reverse=True), range(0, G.number_of_nodes()) ))
+
+### Plot 1
 i=0
 for edge in G.edges():
-  x[i] = (1 - (pr[edge[0]] / maxPR))
-  y[i] = (1 - (pr[edge[1]] / maxPR))
-  z[i] = 1
-  y[i + 1] = (1 - (pr[edge[0]] / maxPR))
-  x[i + 1] = (1 - (pr[edge[1]] / maxPR))
-  z[i + 1] = 1
-  #z[i] = (1 - (pr[edge[1]] / maxPR)) + (1 - (pr[edge[0]] / maxPR))
-  i=i+2
-
-
+  x[i] = y[i + 1] = (1 - (pr[edge[0]] / maxPR))
+  y[i] = x[i + 1] = (1 - (pr[edge[1]] / maxPR))
+  z[i] = z[i + 1] = 1
+  i += 2
 ax1.scatter(x,y, c=z, s=z*20, alpha=0.2, cmap='seismic', vmin=z.min(), vmax=z.max(), linewidths=0)
+ax1.set_xlabel('PR of Source Node')
+ax1.set_ylabel('PR of Target Node')
+ax1.set_title('Page Rank Value')
 
-## order by PR
-
-x = np.zeros(G.number_of_edges() * 2)
-y = np.zeros(G.number_of_edges() * 2)
-z = np.zeros(G.number_of_edges() * 2)
-
-ordered = dict(zip( range(0,G.number_of_nodes()), sorted(G.nodes(), key = lambda node: pr[node], reverse = True) ))
-flip =    dict(zip( sorted(G.nodes(), key = lambda node: pr[node], reverse = True), range(0,G.number_of_nodes()) ))
-
+### Plot 2
 i=0
-for o in ordered.keys():
-  for e in G.edges(ordered[o]):
-    x[i] = o
-    y[i] = flip[e[1]]
-    z[i] = com[e[0]]
-    i=i+1
-
+for edge in G.edges():
+    x[i] = y[i + 1] = flip[edge[0]]
+    y[i] = x[i + 1] = flip[edge[1]]
+    z[i] = com[edge[0]]
+    z[i + 1] = com[edge[1]]
+    i += 2
 ax2.scatter(x,y, c=z, s=20, alpha=0.5, vmin=z.min(), vmax=z.max(), linewidths=0)
+ax2.set_xlabel('Source Node')
+ax2.set_ylabel('Target Node')
+ax2.set_title('Adjacency Matrix ordered by Page Rank Value')
 
-x = np.zeros(G.number_of_edges() * 2)
-y = np.zeros(G.number_of_edges() * 2)
-z = np.zeros(G.number_of_edges() * 2)
-
+### Plot 3
 i=0
-for o in ordered.keys():
-  for e in G.edges(ordered[o]):
-    x[i] = o
-    y[i] = flip[e[1]]
-    if com[ordered[o]] == com[e[1]]:
-      z[i] = 1
-    i=i+1
-
+for edge in G.edges():
+  y[i] = (1 - (pr[edge[0]] / maxPR))
+  x[i] = flip[edge[1]]
+  y[i + 1] = (1 - (pr[edge[1]] / maxPR))
+  x[i + 1] = flip[edge[0]]
+  z[i] = z[i + 1] = 1
+  i += 2
 ax3.scatter(x,y, c=z, s=20, alpha=0.5, vmin=z.min(), vmax=z.max(), linewidths=0)
+ax3.set_xlabel('Source Node Ordered by Page Rank Value')
+ax3.set_ylabel('Target Node Page Rank Values')
+ax3.set_title('Page Rank Order vs. Value')
+
+evc = nx.eigenvector_centrality(G, max_iter=500, tol=.01)
+maxEVC = max(evc.values())
+flip = dict(zip(sorted(G.nodes(), key=lambda node: evc[node], reverse=True), range(0, G.number_of_nodes()) ))
+
+### Plot 4
+i=0
+for edge in G.edges():
+  x[i] = y[i + 1] = (1 - (evc[edge[0]] / maxEVC))
+  y[i] = x[i + 1] = (1 - (evc[edge[1]] / maxEVC))
+  z[i] = z[i + 1] = 1
+  i += 2
+ax4.scatter(x,y, c=z, s=z*20, alpha=0.2, cmap='seismic', vmin=z.min(), vmax=z.max(), linewidths=0)
+ax4.set_xlabel('Eigenvector Centrality of Source Node')
+ax4.set_ylabel('Eigenvector Centrality of Target Node')
+ax4.set_title('Eigenvector Centrality Values')
+
+### Plot 5
+i=0
+for edge in G.edges():
+    x[i] = y[i + 1] = flip[edge[0]]
+    y[i] = x[i + 1] = flip[edge[1]]
+    z[i] = com[edge[0]]
+    z[i + 1] = com[edge[1]]
+    i += 2
+ax5.scatter(x,y, c=z, s=20, alpha=0.5, vmin=z.min(), vmax=z.max(), linewidths=0)
+ax5.set_xlabel('Source Node')
+ax5.set_ylabel('Target Node')
+ax5.set_title('Adjacency Matrix ordered by Eigenvector Centrality Value')
+
+### Plot 6
+i=0
+for edge in G.edges():
+  y[i] = (1 - (evc[edge[0]] / maxEVC))
+  x[i] = flip[edge[1]]
+  y[i + 1] = (1 - (evc[edge[1]] / maxEVC))
+  x[i + 1] = flip[edge[0]]
+  z[i] = z[i + 1] = 1
+  i += 2
+ax6.scatter(x,y, c=z, s=20, alpha=0.5, vmin=z.min(), vmax=z.max(), linewidths=0)
+ax6.set_xlabel('Source Node Ordered by Eigenvector Centrality Value')
+ax6.set_ylabel('Target Node Eigenvector Centrality Values')
+ax6.set_title('Eigenvector Centrality Order vs. Value')
 
 plt.show()
